bebb0c4e6f52148255e9f23a24fe72608e8626a3
vijayst <vijayst@gmail.com>
2019-01-10 08:44:16 +0530

    add a show coverage function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import libCoverage from 'istanbul-lib-coverage';
 
 function Index() {
     const [count, setCount] = useState(0);
@@ -8,12 +9,24 @@ function Index() {
         setCount(count + 1);
     }
 
+    function handleShow() {
+        var map = libCoverage.createCoverageMap({})
+        map.merge(window.__coverage__);
+        Object.keys(map.data).map(key => {
+            console.log(key, map.data[key].toSummary().data);
+        });
+    }
+
+
     return (
         <div>
             {count}
             <button onClick={handleAdd}>+1</button>
+            <button style={{ position: 'fixed', bottom: 0 }} onClick={handleShow}>Show coverage</button>
         </div>
     )
 }
 
 ReactDOM.render(<Index />, document.getElementById("root"));
+
+
